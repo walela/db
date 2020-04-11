f2f716724621be8da6e0b69e2f6f7bd268f6ca2a
Austin Walela <austinmasibo@gmail.com>
2020-04-11 22:27:48 +0300

    let's get this party started

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
 
 function App() {
+  const API = 'https://api.github.com'
+  const [loading, setLoading] = useState(false)
+  const [userData, setUserData] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(`${API}/users/aholachek`)
+      .then(res => {
+        console.log(res.data)
+        setUserData(res.data)
+      })
+      .catch(err => console.error(err))
+      .finally(() => {
+        console.log('done loading')
+        setLoading(false)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>nectar</h1>
       </header>
+      {!loading && userData.length !== 0 ? (
+        <div className='dashboard'>
+          <div className='prof-pic'>
+            <img src={userData.avatar_url} alt='github avatar' />
+          </div>
+        </div>
+      ) : (
+        <h4>Loading...</h4>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
